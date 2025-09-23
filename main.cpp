d06f48cfc8a24cd8cdb080c3db8dcda91a6b562f
Eliseo Zuñiga <davidzunigaobando@gmail.com>
2025-09-23 15:21:27 -0600

    bump

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
-#define _CRTDBG_MAP_ALLOC
-#include <cstdlib>
-#include <crtdbg.h>
 #include <iostream>
 #include <windows.h> 
 #include "Controlador.h"
 int main() {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	SetConsoleCP(1252);          // Para las tildes
 	SetConsoleOutputCP(1252);
 	Controlador* app = new Controlador();
